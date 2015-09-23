1b0aed6df23010f1f690653638f37b6857153a7b
Uma Subramanian <umas@microsoft.com>
2015-09-23 12:16:08 -0700

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,62 @@
+# Sheet Switcher Task Pane Add-in Sample for Excel 2016
+
+_Applies to: Excel 2016_
+
+This task pane add-in provides a way to add new sheets to a workbook via the task pane and easily navigate to the different sheets in Excel 2016. It comes in two flavors: text editor and Visual Studio.
+
+![Sheet Switcher Sample](images/SheetSwitcher_taskpane.PNG)
+
+## Try it out
+### Text editor version
+
+The simplest way to deploy and test your add-in is to copy the files to a network share.
+
+1.  Create a folder on a network share (for example, \\\MyShare\SheetSwitcher) and copy all the files in the Text Editor folder. 
+2.  Edit the <SourceLocation> element of the manifest file so that it points to the share location from step 1. 
+3.  Copy the manifest (SheetSwitcherManifest.xml) to a network share (for example, \\\MyShare\MyManifests).
+4.  Add the share location that contains the manifest as a trusted app catalog in Excel.
+
+    a.  Launch Excel and open a blank spreadsheet.  
+    
+    b.  Choose the **File** tab, and then choose **Options**.
+    
+    c.  Choose **Trust Center**, and then choose the **Trust Center Settings** button.
+    
+    d.  Choose **Trusted App Catalogs**.
+    
+    e.  In the **Catalog Url** box, enter the path to the network share you created in step 1, and then choose **Add Catalog**.
+    
+   f.  Select the **Show in Menu** check box, and then choose **OK**. A message appears to inform you that your settings will be applied the next time you start Office. 
+        
+5.  Test and run the add-in. 
+
+    a.  In the **Insert tab** in Excel 2016, choose **My Add-ins**. 
+    
+    b.  In the **Office Add-ins** dialog box, choose **Shared Folder**.
+    
+    c.  Choose **Sheet Switcher Sample**>**Insert**. The add-in opens in a task pane to the right of the current worksheet, as shown in the following figure. 
+        
+  ![Sheet Switcher Sample](images/SheetSwitcher_taskpane.PNG)
+
+    d.  Click the **Add Sheets!** button. This will add fourteen new sheets to the spreadsheet. Click any of the sheet buttons rendered in the task pane to navigate to that sheet in the workbook.
+        
+
+### Visual Studio version
+1.  Copy the project to a local folder and open the Excel-Add-in-Sheet-Switcher.sln in Visual Studio.
+2.  Press F5 to build and deploy the sample add-in. Excel launches and the add-in opens in a task pane to the right of a blank worksheet, as shown in the following figure. 
+        
+  ![Sheet Switcher Sample](images/SheetSwitcher_taskpane.PNG)
+
+3. Click the **Add Sheets!** button. This will add fourteen new sheets to the spreadsheet. Click any of the sheet buttons rendered in the task pane to navigate to that sheet in the workbook.
+
+
+
+### Learn more
+
+The Excel JavaScript APIs have much more to offer you as you develop add-ins. The following are just a few of the available resources. 
+
+1.  [Excel Add-ins programming overview](https://github.com/OfficeDev/office-js-docs/blob/master/excel/excel-add-ins-programming-overview.md)
+2.  [Snippet Explorer for Excel](http://officesnippetexplorer.azurewebsites.net/#/snippets/excel)
+3.  [Excel Add-ins code samples](https://github.com/OfficeDev/office-js-docs/blob/master/excel/excel-add-ins-code-samples.md) 
+4.  [Excel Add-ins JavaScript API Reference](https://github.com/OfficeDev/office-js-docs/blob/master/excel/excel-add-ins-javascript-reference.md)
+5.  [Build your first Excel Add-in](https://github.com/OfficeDev/office-js-docs/blob/master/excel/build-your-first-excel-add-in.md)
---
+++ Text Editor/Home.js
@@ -0,0 +1,155 @@
+﻿/* Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. 
+    See full license at the bottom of this file. */
+
+(function () {
+	"use strict";
+
+	// The initialize function must be run each time a new page is loaded
+	Office.initialize = function (reason) {
+		$(document).ready(function () {
+			app.initialize();
+
+			// Add a click event handler for the Add Sheets button
+			$("#add-sheets").click(addNewSheets);
+
+			// Add a click event handler for the Refresh button
+			$("#refresh-button").click(createSheetButtons);
+
+			// Create buttons in the task pane for each sheet in the workbook
+			createSheetButtons();
+		});
+	}
+
+
+	function addNewSheets() {
+
+		// Run a batch operation against the Excel object model
+		Excel.run(function (ctx) {
+			// Create a proxy object for the worksheets collection
+			var worksheets = ctx.workbook.worksheets;
+
+			// Add 14 sheets to the workbook
+			for (var i = 2; i <= 15; i++) {
+				// Queue commands to add new sheets to the workbook
+				worksheets.add("Sheet" + i);
+			}
+
+			//Disable the button
+			$('#add-sheets').prop('disabled', true);
+
+
+			//Run the queued-up commands, and return a promise to indicate task completion
+			return ctx.sync();
+
+		})
+		.then(function () {
+			// Now that we have sheets, create buttons for each sheet
+			// in the taskpane to enable switching
+			createSheetButtons();
+		})
+		.catch(function (error) {
+			// Always be sure to catch any accumulated errors that bubble up from the Excel.run execution
+			app.showNotification("Error: " + error);
+			console.log("Error: " + error);
+			if (error instanceof OfficeExtension.Error) {
+				console.log("Debug info: " + JSON.stringify(error.debugInfo));
+			}
+		});
+	}
+
+
+	function createSheetButtons() {
+
+		// Remove the existing buttons
+		$("#buttons-div").empty();
+
+		// Run a batch operation against the Excel object model
+		Excel.run(function (ctx) {
+			// Create a proxy object for the worksheets collection 
+			var worksheets = ctx.workbook.worksheets;
+
+			// Queue a command to load the name property of each worksheet
+			worksheets.load("name");
+
+			//Run the queued-up commands, and return a promise to indicate task completion
+			return ctx.sync()
+				.then(function () {
+					//create a button for each sheet in the task pane
+					for (var i = 0; i < worksheets.items.length; i++) {
+						var buttonName = worksheets.items[i].name;
+						var $input = $('<input type="button" class="ms-Button" value=' + buttonName + '>');
+						$input.appendTo($("#buttons-div"));
+						// Add a click event handler for the button
+						(function (buttonName) {
+							$input.click(function (e) {
+								makeActiveSheet(buttonName);
+							});
+						})(buttonName);
+					}
+				});
+		})
+		.catch(function (error) {
+			// Always be sure to catch any accumulated errors that bubble up from the Excel.run execution
+			app.showNotification("Error: " + error);
+			console.log("Error: " + error);
+			if (error instanceof OfficeExtension.Error) {
+				console.log("Debug info: " + JSON.stringify(error.debugInfo));
+			}
+		});
+	}
+
+
+	function makeActiveSheet(buttonName) {
+
+		// Run a batch operation against the Excel object model
+		Excel.run(function (ctx) {
+			// Create a proxy object for the worksheets collection 
+			var worksheets = ctx.workbook.worksheets;
+
+			// Queue a command to get the sheet with the name of the clicked button
+			var clickedSheet = worksheets.getItem(buttonName);
+
+			// Queue a command to insert the sheet name into a cell for easy viewing
+			clickedSheet.getCell(0, 0).values = buttonName;
+
+			// Queue a command to activate the clicked sheet
+			clickedSheet.activate();
+
+			//Run the queued-up commands, and return a promise to indicate task completion
+			return ctx.sync();
+		})
+		.catch(function (error) {
+			// Always be sure to catch any accumulated errors that bubble up from the Excel.run execution
+			app.showNotification("Error: " + error);
+			console.log("Error: " + error);
+			if (error instanceof OfficeExtension.Error) {
+				console.log("Debug info: " + JSON.stringify(error.debugInfo));
+			}
+		});
+	}
+})();
+
+/*
+Excel-Add-in-JS-QuarterlySalesReportManifest, https://github.com/OfficeDev/Excel-Add-in-JS-QuarterlySalesReport
+
+Copyright (c) Microsoft Corporation
+
+All rights reserved. 
+
+MIT License:
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+associated documentation files (the "Software"), to deal in the Software without restriction, including 
+without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the 
+following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial 
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT 
+SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE 
+USE OR OTHER DEALINGS IN THE SOFTWARE. 
+*/
